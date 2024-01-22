0e34076745e3aa1c4d64516fd54acd01d547f6c3
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-01-23 00:13:51 +0500

    Fake News Detection Project added

---
+++ Python_Projects/AI Assistant (JARVIS)/jarvis.py
@@ -0,0 +1,41 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# speak("This is jarvis, ai assisstant")
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak(Time)
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
+
+def wish_me():
+    speak("Welcome back Sir!")
+    speak("the current time is")
+    time()
+    speak("the current date is")
+    date()
+    hour = datetime.datetime.now().hour
+    if hour >=6 and hour <12:
+        speak("Good Morning Sir!")
+    elif hour >=12 and hour <18:
+        speak("Good afternoon Sir!")
+    elif hour >=18 and hour <24:
+        speak("Good evening Sir!")
+    else:
+        speak("Good night Sir!")    
+    speak("jarvis at your service, please tell me how can i help you?")
+
+wish_me()
