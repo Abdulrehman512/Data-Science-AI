60f9aba9c3360334dfdf64a156c31b5db8a79fd5
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-02-03 15:57:18 +0500

    Python Crash Course Chapter 3 Practice

--- Python_Practice (Python Crash Course 3rd Edition)/Coding_Practice/Chapter_3/practice.py
+++ Python_Practice (Python Crash Course 3rd Edition)/Coding_Practice/Chapter_3/practice.py
@@ -2,3 +2,23 @@
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+# Accessing Elements in a List
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+# Index Positions Start at 0, Not 1
+
+print(bicycles[1])
+print(bicycles[3])
+
+# This code returns the second and fourth bicycles in the list:
+
+print(bicycles[-1])
+
+# Using Individual Values from a List
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
