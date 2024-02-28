0779a9331db75bc23557b416d8516017961108ec
Abdul Rehman <abdul.rehman11992@gmail.com>
2024-02-28 14:36:48 +0500

    Chapter 3 Coding Practice

--- Python_Practice (Python Crash Course 3rd Edition)/Coding_Practice/Chapter_3/practice.py
+++ Python_Practice (Python Crash Course 3rd Edition)/Coding_Practice/Chapter_3/practice.py
@@ -94,3 +94,43 @@ too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
 print(motorcycles)
 print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+# Sorting a List Permanently with the sort() Method
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+# Sorting a List Temporarily with the sorted() Function
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+# Printing a List in Reverse Order
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+# Finding the Length of a List
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
+
+# Avoiding Index Errors When Working with Lists
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles[3])
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles[-1])
